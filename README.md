385c3f4c56bdba5d0ae0584735c5336b00ffb2cc
Mohamed Anas <mabo.tn@outlook.com>
2017-10-28 00:32:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Bucket
+
+Bucket is an objects storage server built in NodeJS to serve apps as a standalone microservice via RESTful API.
+
+The bucket supports Gzip compression for static content and access logs.
+
+## How to deploy using Docker
+
+In your docker host, run a new container
+
+`docker run -d --name my-bucket devrecipe/bucket`
+
+And retrieve the container's IP address
+
+`docker inspect --format '{{.NetworkSettings.IPAddress}}' my-bucket`
+
+## How to use the API
+
+1.  Retrieve an object from the bucket
+
+    Send a GET request `/oject/*` where `*` is the object id, the output should be the file from server.
+
+2.  Add a new object to the bucket
+
+    Send a POST request `/upload` where `*` is the object id. request content-type should be `multipart form data` with a field named `file`.
+
+    Output should be a JSON with the object id (`filename`) and its mime type, encoding and size.
+
+3.  Delete existin object from the bucket
+    
+    Send a DELETE request `/oject/*` where `*` is the object id, the output should be a json with this message `The requested object is deleted`.
+
+In case of an error, (like the requested object doesn't exist) a JSON error will be returned
+
+```
+{
+    result: 404,
+    message: "The requested object doesn't exist"
+}
+```
+
+## Securing the bucket
+
+Coming soon, meanwhile we'd be happy to hear from you some feedbacks
+
+## Contributors
+
+* Mohamed Anas Ben Othman
+
+## License
+
+&copy; 2017 - MIT License
---
+++ server.js
@@ -0,0 +1,92 @@
+var compression = require('compression')
+var bodyParser = require('body-parser')
+var express = require('express')
+var multer = require('multer')
+var morgan = require('morgan')
+var crypto = require('crypto')
+var path = require('path')
+var hat = require('hat')
+var fs = require('fs')
+
+var app = express()
+var port = process.env.PORT || 1337
+var uploader = multer({
+    storage: multer.diskStorage({
+        destination: function(req, file, cb) {
+            cb(null, './objects/')
+        },
+        filename: function(req, file, cb) {
+            crypto.pseudoRandomBytes(16, function (err, raw) {
+                if (err) return cb(err)
+                cb(null, hat() + path.extname(file.originalname))
+            })
+        }
+    })
+})
+
+app.use(compression())
+app.use(morgan('tiny'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/object/:fileId', (req, res) => {
+    var file = process.cwd() + '/objects/' + req.params.fileId
+
+    fs.exists(file, function(exists) {
+        if (exists) {
+            res.sendFile(file)
+        } else {
+            res.status(404).json({
+                result: 404,
+                message: "The requested object doesn't exist"
+            })
+        }
+    })
+})
+
+app.post('/upload', uploader.single('file'), function (req, res) {
+    res.json({
+        filename: req.file.filename,
+        mimetype: req.file.mimetype,
+        encoding: req.file.encoding,
+        size: req.file.size
+    })
+})
+
+app.delete('/onject/:fileId', (req, res) => {
+    var file = process.cwd() + '/objects/' + req.params.fileId
+
+    fs.exists(file, function (exists) {
+        if (exists) {
+            fs.unlink(file, function(err) {
+                if (err) {
+                    res.status(200).json({
+                        result: 500,
+                        message: "The requested object cannot be deleted"
+                    })
+                } else {
+                    res.status(200).json({
+                        result: 200,
+                        message: "The requested object is deleted"
+                    })
+                }
+            })
+        } else {
+            res.status(404).json({
+                result: 404,
+                message: "The requested object doesn't exist"
+            })
+        }
+    })
+})
+
+app.all('/*', (req, res) => {
+    res.status(400).json({
+        result: 400,
+        message: "The requested endpoint doesn't exist"
+    })
+})
+
+app.listen(port, '0.0.0.0', function() {
+    console.log('Bucket running at port: ' + port)
+})
